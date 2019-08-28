e0345304e64ba9eac78eb24a36710a4692576634
reddynie11 <reddy.nie11@gmail.com>
2019-08-29 01:25:49 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>New App</h1>
-    </div>
-  );
+import Todos from './Todos';
+
+class App extends React.Component{
+  state={
+    todos:[
+      {id:1, content:'Learn React JS'},
+      {id:2, content:'Build React JS Project'}
+    ]
+  }
+ 
+  render(){
+    return(
+      <div className="container" >
+        <h2 className="center blue-text" > ToDO's</h2>
+        <Todos todos={this.state.todos} delTodo={this.delTodo}/>
+      </div>
+      
+    )
+  }
 }
 
 export default App;
